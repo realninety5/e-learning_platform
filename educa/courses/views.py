d6b6665d827b0c6b6be1172beee0fb4699cc4044
Okah <kingsleyog95@gmail.com>
2021-01-05 14:08:44 +0100

    Added formset for adding multiple modules at once

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .form import ModuleFormset
+from django.views.generic.base import TemplateResponseMixin, View
 from django.contrib.auth.mixins import LoginRequiredMixin,\
         PermissionRequiredMixin
 from .models import Course
@@ -10,7 +12,7 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 class ManageCourseListView(ListView):
     model = Course
-    template_name = 'courses/manage/course/.list.html'
+    template_name = 'courses/manage/course/list.html'
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -50,3 +52,26 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+    def get_formset(self, data=None):
+        return ModuleFormset(instance=self.course,
+                             data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk,
+                                        owner=request.user)
+        return super().dispatch(request, pk)
+    def get(self, request, *args, **kwargs):
+        formset = self,get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset}) 
